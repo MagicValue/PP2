6e17d14ac63923918735e9f18b87ce92c26a6666
MagicValue <sanek1023jr12@yandex.by>
2022-05-03 13:35:10 +0300

    doesn't work

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,91 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.engine.jdbc.spi.ResultSetReturn;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Util util = new Util();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        String SQL = "CREATE TABLE User(id int increment," +
+                " name varchar(55)," +
+                " lastName varchar(55)," +
+                " age int);";
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeQuery(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        String SQL = "DROP TABLE User;";
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            String SQL = "INSERT INTO User VALUES(name, lastName, age);";
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            String SQL = "SELECT * FROM User WHERE id = id;";
+            Statement statement = util.getConnection().createStatement();
+            statement.executeQuery(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try {
+            String SQL = "SELECT * FROM User;";
+            Statement statement = util.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String SQL = "TRUNCATE TABLE User;";
+        try {
+            Statement statement = util.getConnection().createStatement();
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
